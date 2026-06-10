bd05a6f6b4969ea2456d52ee028a6aa1d8a066dd
Zemiro014 <zemiro014@gmail.com>
2026-06-10 11:57:17 -0300

    document-etraction-service: new service created

---
+++ document-extraction-service/src/main/java/com/jeronimo/document_extraction_service/DocumentExtractionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jeronimo.document_extraction_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentExtractionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentExtractionServiceApplication.class, args);
+	}
+
+}
---
+++ document-extraction-service/src/test/java/com/jeronimo/document_extraction_service/DocumentExtractionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeronimo.document_extraction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentExtractionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
